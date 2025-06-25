6ab9f6eb5dff8fa7dcf2df439322adb65dddca4a
Oriana Casas <146871830+Aainor@users.noreply.github.com>
2025-06-25 15:30:43 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,26 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDKq4YA4Qk9ceD1XQcaEZnPMth1lqSN4rE",
-  authDomain: "mudanzas-caba.firebaseapp.com",
-  databaseURL: "https://mudanzas-caba-default-rtdb.firebaseio.com",
-  projectId: "mudanzas-caba",
-  storageBucket: "mudanzas-caba.firebasestorage.app",
-  messagingSenderId: "292614458356",
-  appId: "1:292614458356:web:8daa5f7ac8b48251c187a1",
-  measurementId: "G-G9B7SQG3MG"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 // DOM Elements
 const loginForm = document.getElementById('login');
 const registerForm = document.getElementById('register');
